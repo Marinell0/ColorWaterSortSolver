31d418a45f3da9ebc12ca145ea65e0ce34f66c6b
Gabriel Marinello Moura Leite <gabrielmarinello6@gmail.com>
2023-01-08 17:54:20 -0300

    Add size to vial class
    Signed-off-by: Gabriel Marinello Moura Leite <gabrielmarinello6@gmail.com>

--- src/vials/AbstractVial.java
+++ src/vials/AbstractVial.java
@@ -5,24 +5,29 @@ import java.util.stream.Stream;
 
 public abstract class AbstractVial<T> {
     protected ArrayList<T> colors = new ArrayList<>(4);
+    protected int size = 0;
 
     protected AbstractVial(T first, T second, T third, T fourth) {
         this(first, second, third);
         set(4, fourth);
+        this.size = 4;
     }
 
     protected AbstractVial(T first, T second, T third) {
         this(first, second);
         set(2, third);
+        this.size = 3;
     }
 
     protected AbstractVial(T first, T second) {
         this(first);
         set(1, second);
+        this.size = 2;
     }
 
     protected AbstractVial(T first) {
         set(0, first);
+        this.size = 1;
     }
 
     public abstract Stream<T> colors();
