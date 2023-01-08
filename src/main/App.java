b728528a0caf5225e0139532c6d906673dfefa9e
Gabriel Marinello Moura Leite <gabrielmarinello6@gmail.com>
2023-01-08 17:22:38 -0300

    Move App to main
    Signed-off-by: Gabriel Marinello Moura Leite <gabrielmarinello6@gmail.com>

--- src/main/App.java
+++ src/main/App.java
@@ -1,7 +1,9 @@
+package main;
+
 import vials.EnumVial;
 
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         EnumVial[] vials = new EnumVial[8];
         for (int i = 0; i < vials.length; i++) {
             vials[i] = new EnumVial();
