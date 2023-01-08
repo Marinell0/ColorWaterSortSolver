40803d3ee7807eff95c5c8abc10dd65bdbccd61a
Gabriel Marinello Moura Leite <gabrielmarinello6@gmail.com>
2023-01-08 17:52:32 -0300

    Add constructors for all sizes of vials
    Signed-off-by: Gabriel Marinello Moura Leite <gabrielmarinello6@gmail.com>

--- src/vials/AbstractVial.java
+++ src/vials/AbstractVial.java
@@ -7,10 +7,22 @@ public abstract class AbstractVial<T> {
     protected ArrayList<T> colors = new ArrayList<>(4);
 
     protected AbstractVial(T first, T second, T third, T fourth) {
-        colors.add(first);
-        colors.add(second);
-        colors.add(third);
-        colors.add(fourth);
+        this(first, second, third);
+        set(4, fourth);
+    }
+
+    protected AbstractVial(T first, T second, T third) {
+        this(first, second);
+        set(2, third);
+    }
+
+    protected AbstractVial(T first, T second) {
+        this(first);
+        set(1, second);
+    }
+
+    protected AbstractVial(T first) {
+        set(0, first);
     }
 
     public abstract Stream<T> colors();
@@ -19,6 +31,16 @@ public abstract class AbstractVial<T> {
 
     public void set(int pos, T value) {
         colors.set(pos, value);
+    }
+
+    public void transfer(AbstractVial<T> other) {
+        for (int i = 0; i < colors.size(); i++) {
+            if (colors.get(i).equals(emptyColor())) {
+                colors.set(i, other.colors.get(i));
+                other.colors.set(i, other.emptyColor());
+                break;
+            }
+        }
     }
 
     public boolean isEmpty() {
