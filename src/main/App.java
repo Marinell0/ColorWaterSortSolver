8912b8fb2adcbad93a36e9f4cc23976bad9fdf44
Gabriel Marinello Moura Leite <gabrielmarinello6@gmail.com>
2023-01-08 20:46:03 -0300

    Print initial board on main
    Signed-off-by: Gabriel Marinello Moura Leite <gabrielmarinello6@gmail.com>

--- src/main/App.java
+++ src/main/App.java
@@ -1,10 +1,16 @@
 package main;
 
+import java.util.Arrays;
+
+import reader.Board;
 import reader.RandomBoard;
+import vials.Vial;
 
 public class App {
     public static void main(String[] args) {
-        RandomBoard board = new RandomBoard(2);
-        board.game();
+        Board board = new RandomBoard(2);
+        Vial[] vials = board.game();
+
+        Arrays.stream(vials).forEach(System.out::println);
     }
 }
