57b9ade04cfa0d62537d34335e9489a80e4f7b30
Gabriel Marinello Moura Leite <gabrielmarinello6@gmail.com>
2023-01-08 21:58:41 -0300

    Print vials on main
    Signed-off-by: Gabriel Marinello Moura Leite <gabrielmarinello6@gmail.com>

--- src/main/App.java
+++ src/main/App.java
@@ -4,13 +4,11 @@ import java.util.Arrays;
 
 import reader.Board;
 import reader.RandomBoard;
-import vials.Vial;
 
 public class App {
     public static void main(String[] args) {
-        Board board = new RandomBoard(2);
-        Vial[] vials = board.game();
+        Board board = new RandomBoard(0);
 
-        Arrays.stream(vials).forEach(System.out::println);
+        Arrays.stream(board.getVials()).forEach(System.out::println);
     }
 }
