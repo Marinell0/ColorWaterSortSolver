fa7abd454d1418962891e714ce316aa08085edf4
Gabriel Marinello Moura Leite <gabrielmarinello6@gmail.com>
2023-01-08 22:22:14 -0300

    Add RGB to colors
    Signed-off-by: Gabriel Marinello Moura Leite <gabrielmarinello6@gmail.com>

--- src/colorenum/Colors.java
+++ src/colorenum/Colors.java
@@ -1,23 +1,49 @@
 package colorenum;
 
 public enum Colors {
-    BLUE,
-    CYAN,
-    LIGHT_GREEN,
-    GREEN,
-    DARK_GREEN,
-    YELLOW,
-    ORANGE,
-    BROWN,
-    RED,
-    PINK,
-    PURPLE,
-    GREY,
-    EMPTY;
+    BLUE(0, 0, 255),
+    CYAN(0, 255, 255),
+    LIGHT_GREEN(144, 238, 144),
+    GREEN(0, 255, 0),
+    DARK_GREEN(0, 100, 0),
+    YELLOW(255, 255, 0),
+    ORANGE(255, 165, 0),
+    BROWN(165, 42, 42),
+    RED(255, 0, 0),
+    PINK(255, 192, 203),
+    PURPLE(128, 0, 128),
+    GREY(128, 128, 128),
+    EMPTY(0, 0, 0);
+
+    private int r;
+    private int g;
+    private int b;
+
+    private Colors(int r, int g, int b) {
+        this.r = r;
+        this.g = g;
+        this.b = b;
+    }
 
     private static Colors[] colors = Colors.values();
 
     public static Colors[] getColors() {
         return colors;
+    }
+
+    public String toString() {
+        return this.consoleFGColor() + this.name() + this.consoleColorReset();
+    }
+    
+    public String consoleFGColor() {
+        return "\u001B[38;2;" + r + ";" + g + ";" + b + "m";
+    }
+
+    public String consoleBGColor() {
+        return "\u001B[48;2;" + r + ";" + g + ";" + b + "m";
+    }
+
+    public String consoleColorReset() {
+        return "\u001B[0m";
     }
 }
